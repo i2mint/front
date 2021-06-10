6fc7e16cb88cf8298010d152ff4c43d93defdac3
owenlloyd <owen7lloyd@gmail.com>
2021-06-10 11:18:27 -0400

    Example to create a dacc from local_kgl in odat

---
+++ front/examples/kaggle_front.py
@@ -0,0 +1,29 @@
+import os
+from odat.mdat.local_kgl import mk_dacc
+import dill
+from front.session_state import _get_state, _SessionState
+
+
+def kaggle(
+    zip_path: type(lambda a: a),
+    annots_path: str,
+    wrangle_func: type(lambda a: a),
+    extension: str = '.wav',
+    key_path: str = '',
+):
+    wrangle_func = dill.load(wrangle_func)
+    return mk_dacc(
+        zip_path, annots_path, wrangle_func, extension=extension, key_path=key_path,
+    )
+
+
+funcs = [kaggle]
+
+if __name__ == '__main__':
+    from front.base import dispatch_funcs
+
+    print('file: {}'.format(os.path.realpath(__file__)))
+
+    app = dispatch_funcs(funcs)
+
+    app()
