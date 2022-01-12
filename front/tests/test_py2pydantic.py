e0e5a4be5e85b8cf39d0c5f0a1421fc8f6eabbcb
sylvainbonnot <sppbonnot@gmail.com>
2022-01-12 15:19:48 +0100

    added tests

--- front/tests/test_py2pydantic.py
+++ front/tests/test_py2pydantic.py
@@ -3,7 +3,11 @@ import pytest
 from i2 import Sig
 from pydantic import create_model
 from i2.tests.objects_for_testing import formula1
-from front.scrap.py2pydantic import func_to_pyd_model_specs, func_to_pyd_input_model_cls
+from front.scrap.py2pydantic import (
+    func_to_pyd_model_specs,
+    func_to_pyd_input_model_cls,
+    pyd_func_ingress_template,
+)
 import typing
 
 
@@ -33,3 +37,12 @@ def test_func_to_pyd_input_model_cls():
         "required": ["w", "x"],
     }
     assert result_schema == expected_schema
+
+
+def test_pyd_func_ingress_template():
+    wrapped_func_sig = Sig(formula1)
+    pyd_model = func_to_pyd_input_model_cls(formula1, dflt_type=typing.Any)
+    input_model_instance = pyd_model(w=1, x=2, y=3)
+    result = pyd_func_ingress_template(input_model_instance, wrapped_func_sig)
+    expected = ((1,), {"x": 2.0, "y": 3, "z": 1})
+    assert result == expected
