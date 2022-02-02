4f6984eda4f7ec9d6ae01be49c4ea9392093a8f6
sylvainbonnot <sppbonnot@gmail.com>
2022-02-02 18:13:51 +0100

    pydantic output

--- front/scrap/pydantic_wrap.py
+++ front/scrap/pydantic_wrap.py
@@ -8,10 +8,16 @@ import streamlit as st
 import streamlit_pydantic as sp
 from opyrator.ui.streamlit_ui import InputUI
 from opyrator import Opyrator
-from pydantic import BaseModel
+from pydantic import BaseModel, create_model
 
 
-class SimplePageFuncPydantic(BasePageFunc):
+def pydantic_model_from_type(mytype, name="Output", field_name="result"):
+    model = create_model(name, result=(mytype, ...))
+
+    return model
+
+
+class SimplePageFuncPydanticWrite(BasePageFunc):
     def __call__(self, state):
         self.prepare_view(state)
         mymodel = func_to_pyd_input_model_cls(self.func)
@@ -19,11 +25,33 @@ class SimplePageFuncPydantic(BasePageFunc):
             self.func.__name__
         )  # check in sig, dag, lined a better way, i2, may be displayed name: name_of_obj
 
-        data = sp.pydantic_input(key=f'my_form_{name}', model=mymodel)
+        data = sp.pydantic_input(key=f"my_form_{name}", model=mymodel)
 
         if data:
 
             st.write(self.func(**data))
 
 
-DFLT_CONFIGS = {'page_factory': SimplePageFuncPydantic}
+class SimplePageFuncPydanticWithOutput(BasePageFunc):
+    def __call__(self, state):
+        self.prepare_view(state)
+        mymodel = func_to_pyd_input_model_cls(self.func)
+        mytype = self.func.__annotations__["return"]
+        output_model = pydantic_model_from_type(mytype)
+
+        name = (
+            self.func.__name__
+        )  # check in sig, dag, lined a better way, i2, may be displayed name: name_of_obj
+
+        data = sp.pydantic_input(key=f"my_form_{name}", model=mymodel)
+
+        if data:
+            func_result = self.func(**data)
+
+            instance = output_model(result=func_result)
+
+            st.write(instance)
+            sp.pydantic_output(instance)
+
+
+DFLT_CONFIGS = {"page_factory": SimplePageFuncPydanticWrite}
