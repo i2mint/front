a52026b188fe02aece287812cdaed58ff690a0e0
sylvainbonnot <sppbonnot@gmail.com>
2022-02-04 16:52:34 +0100

    flat_dispatch

---
+++ front/scrap/flat_dispatch_app.py
@@ -0,0 +1,33 @@
+from streamlitfront import dispatch_funcs
+from front.scrap.pydantic_wrap import SimplePageFuncPydanticWithOutput
+from typing import Optional, Dict, List
+from streamlit_pydantic.types import FileContent
+from pydantic import Field, BaseModel
+from dol import Store
+
+mall = dict()
+
+
+def get_data(x: Dict[str, int]) -> List[str]:
+    mall["salary"] = x
+    return list(x.keys())
+
+
+funcs = [get_data]
+
+configs = {"page_factory": SimplePageFuncPydanticWithOutput}
+
+
+class SelectionValue(str, Enum):
+    FOO = "foo"
+    BAR = "bar"
+
+
+class ExampleModel(BaseModel):
+    single_selection: SelectionValue = Field(
+        ..., description="Only select a single item from a set."
+    )
+
+
+app = dispatch_funcs(funcs, configs=configs)
+app()
---
+++ front/scrap/flat_dispatch_app_2_5.py
@@ -0,0 +1,29 @@
+import streamlit_pydantic as sp
+import streamlit as st
+from typing_extensions import TypedDict
+from pydantic import create_model_from_typeddict
+from enum import Enum
+
+
+mall = {"x": {"xs_1": 3, "xs_2": 5}, "fvs": {"fv_1": [1, 2, 3], "fv_2": [4, 5, 6]}}
+
+
+def mk_Enum_from_store(store, key):
+    choices = store[key].keys()
+    choice = Enum(f"DynamicEnum_{key}", {item: item for item in choices})
+    return choice
+
+
+def mk_Input_for_store_keys(store, keys):
+    vals = {key: mk_Enum_from_store(store, key) for key in keys}
+    choices_dict = TypedDict("Choices", vals)
+    Input = create_model_from_typeddict(choices_dict)
+
+    return Input
+
+
+my_input = mk_Input_for_store_keys(store=mall, keys=["x", "fvs"])
+
+data = sp.pydantic_input(key="my_form", model=my_input)
+if data:
+    st.write(data)
