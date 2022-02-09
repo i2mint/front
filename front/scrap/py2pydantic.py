acc72499a482826d09db8d0078a379bea1bc09a3
Thor Whalen <thorwhalen1@gmail.com>
2022-02-09 08:00:34 -0800

    refactor: move py2pydantic

--- front/scrap/py2pydantic.py
+++ front/scrap/py2pydantic.py
@@ -4,8 +4,6 @@ Moved to seperate py2pydantic package
 
 from contextlib import suppress
 
-with suppress(ModuleNotFoundError, ImportError):
-    from opyratorfront.py2pydantic import *
-    from warnings import warn
+from warnings import warn
+warn('Moved to front.py2pydantic: }', DeprecationWarning)
 
-    warn('Moved to seperate py2pydantic package', DeprecationWarning)
