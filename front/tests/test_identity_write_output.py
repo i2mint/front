f67887b22731d4be529f90a44a3d3898d07f3ed7
Christian Avart <cavart28@gmail.com>
2022-02-14 14:16:00 -0800

    Refactor tests, added related todos

--- front/tests/test_identity_write_output.py
+++ front/tests/test_identity_write_output.py
@@ -1,18 +1,20 @@
 
-from seleniumbase import BaseCase
-import time
+# TODO: needs to be headless and minimum dependency
 
-
-
-class ComponentsTest(BaseCase):
-    def test_basic(self):
-
-        # open the app and take a screenshot
-        self.open("http://localhost:8501")
-
-        time.sleep(5)  
-        self.check_window(name="first_test", level=2)
-
-        
-        self.assert_text("Identity")
+# from seleniumbase import BaseCase
+# import time
+#
+#
+#
+# class ComponentsTest(BaseCase):
+#     def test_basic(self):
+#
+#         # open the app and take a screenshot
+#         self.open("http://localhost:8501")
+#
+#         time.sleep(5)
+#         self.check_window(name="first_test", level=2)
+#
+#
+#         self.assert_text("Identity")
 
