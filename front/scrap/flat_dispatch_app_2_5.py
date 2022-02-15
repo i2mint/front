de53b086afb1a2b8ed7d09a09ed9087fea529b38
sylvainbonnot <sppbonnot@gmail.com>
2022-02-15 16:28:57 +0100

    update 2.5

--- front/scrap/flat_dispatch_app_2_5.py
+++ front/scrap/flat_dispatch_app_2_5.py
@@ -3,17 +3,27 @@ import streamlit as st
 from typing_extensions import TypedDict
 from pydantic import create_model_from_typeddict
 from enum import Enum
+from dol import Files
+
+rootdir = "/Users/sylvain/Desktop/stuff"
 
 
-mall = {"x": {"xs_1": 3, "xs_2": 5}, "fvs": {"fv_1": [1, 2, 3], "fv_2": [4, 5, 6]}}
+mall = {
+    "x": {"xs_1": 3, "xs_2": 5},
+    "fvs": {"fv_1": [1, 2, 3], "fv_2": [4, 5, 6]},
+    "files": Files(rootdir),
+}
 
 
-def mk_Enum_from_store(store, key):
+def mk_Enum_from_store(store, key):  # no caps in funcs
     choices = store[key].keys()
     choice = Enum(f"DynamicEnum_{key}", {item: item for item in choices})
     return choice
 
 
+# differentiate between store and mall
+
+# may be replace (store, keys) by a simple dict
 def mk_Input_for_store_keys(store, keys):
     vals = {key: mk_Enum_from_store(store, key) for key in keys}
     choices_dict = TypedDict("Choices", vals)
@@ -22,8 +32,19 @@ def mk_Input_for_store_keys(store, keys):
     return Input
 
 
-my_input = mk_Input_for_store_keys(store=mall, keys=["x", "fvs"])
+# class Input(BaseModel):
+#     x:dict
+#     fvs: dict
 
-data = sp.pydantic_input(key="my_form", model=my_input)
+my_keys = list(mall)
+my_input = mk_Input_for_store_keys(store=mall, keys=my_keys)
+
+data = sp.pydantic_form(key="my_form", model=my_input)
 if data:
     st.write(data)
+
+# InputObject -> (args, kwargs)-- func --> result -> OutputObject
+
+# f: (x:type) -> result
+#
+# dispatch by type
