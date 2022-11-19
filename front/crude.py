3f13e1f72a67aefafe232144dc830e47e887aeb8
Thor Whalen <thorwhalen1@gmail.com>
2022-11-18 19:44:11 -0500

    feat: crudify_based_on_names

--- front/crude.py
+++ front/crude.py
@@ -67,9 +67,9 @@ def auto_key(*args, **kwargs) -> KT:
     >>> auto_key()
     ''
     """
-    args_str = ','.join(map(str, args))
-    kwargs_str = ','.join(map(lambda kv: f'{kv[0]}={kv[1]}', kwargs.items()))
-    return ','.join(filter(None, [args_str, kwargs_str]))
+    args_str = ",".join(map(str, args))
+    kwargs_str = ",".join(map(lambda kv: f"{kv[0]}={kv[1]}", kwargs.items()))
+    return ",".join(filter(None, [args_str, kwargs_str]))
 
 
 @wrap_kvs(data_of_obj=dill.dumps, obj_of_data=dill.loads)
@@ -81,7 +81,7 @@ class DillFiles(Files):
 
 def mk_mall_of_dill_stores(store_names=Iterable[StoreName], rootdir=None):
     """Make a mall of DillFiles stores"""
-    rootdir = rootdir or mk_tmp_dol_dir('crude')
+    rootdir = rootdir or mk_tmp_dol_dir("crude")
     if isinstance(store_names, str):
         store_names = store_names.split()
 
@@ -101,12 +101,12 @@ from i2 import call_forgivingly
 def _validate_function_keyword_only_params(func, allowed_params: Iterable, obj_name):
     if func is not None:
         if not callable(func):
-            raise TypeError(f'{obj_name} should be callable: {func}')
+            raise TypeError(f"{obj_name} should be callable: {func}")
         sig = Sig(func)
         if not all(kind == Parameter.KEYWORD_ONLY for kind in sig.kinds.values()):
-            raise TypeError(f'All params of {obj_name} must be keyword-only. {sig}')
+            raise TypeError(f"All params of {obj_name} must be keyword-only. {sig}")
         if not set(sig.names).issubset(allowed_params):
-            raise TypeError(f'All params of {obj_name} must be in {allowed_params}')
+            raise TypeError(f"All params of {obj_name} must be in {allowed_params}")
 
 
 def _store_on_output(*args, _store_on_ouput_args, **kwargs):
@@ -125,7 +125,7 @@ def _store_on_output(*args, _store_on_ouput_args, **kwargs):
     if not save_name and empty_name_callback:
         assert callable(
             empty_name_callback
-        ), f'empty_name_callback must be callable: {empty_name_callback}'
+        ), f"empty_name_callback must be callable: {empty_name_callback}"
         empty_name_callback()
     args, kwargs = sig.args_and_kwargs_from_kwargs(arguments)
     output = func(*args, **kwargs)
@@ -147,8 +147,8 @@ def store_on_output(
     func=None,
     *,
     store=None,
-    save_name_param='save_name',
-    add_store_to_func_attr='output_store',
+    save_name_param="save_name",
+    add_store_to_func_attr="output_store",
     empty_name_callback: Callable[[], Any] = None,
     auto_namer: Callable[..., str] = None,
     output_trans: Callable[..., Any] = None,
@@ -237,17 +237,20 @@ def store_on_output(
 
     """
     save_name_param_obj = Parameter(
-        name=save_name_param, kind=Parameter.KEYWORD_ONLY, default='', annotation=str,
+        name=save_name_param,
+        kind=Parameter.KEYWORD_ONLY,
+        default="",
+        annotation=str,
     )
     _validate_function_keyword_only_params(
-        auto_namer, ['output', 'arguments'], obj_name='auto_namer'
+        auto_namer, ["output", "arguments"], obj_name="auto_namer"
     )
     _validate_function_keyword_only_params(
-        output_trans, ['save_name', 'output', 'arguments'], obj_name='output_trans'
+        output_trans, ["save_name", "output", "arguments"], obj_name="output_trans"
     )
     if output_trans:
         assert callable(output_trans) and set(Sig(output_trans).names).issubset(
-            ['save_name', 'output', 'arguments']
+            ["save_name", "output", "arguments"]
         )
     sig = Sig(func) + [save_name_param_obj]
 
@@ -290,7 +293,7 @@ def prepare_for_crude_dispatch(
     include_stores_attribute: bool = False,
     output_store: Optional[Union[Mapping, str]] = None,
     # the arguments below only apply if output_store is given
-    save_name_param: str = 'save_name',
+    save_name_param: str = "save_name",
     empty_name_callback: Callable[[], Any] = None,
     auto_namer: Callable[..., str] = None,
     output_trans: Callable[..., Any] = None,
@@ -495,7 +498,9 @@ def prepare_for_crude_dispatch(
         )
 
         ingress = Ingress(
-            inner_sig=sig, kwargs_trans=kwargs_trans, outer_sig=outer_sig,
+            inner_sig=sig,
+            kwargs_trans=kwargs_trans,
+            outer_sig=outer_sig,
         )
 
     wrapped_f = wrap(func, ingress=ingress)
@@ -504,25 +509,25 @@ def prepare_for_crude_dispatch(
         wrapped_f.store_for_param = store_for_param
 
     if output_store is not None:
-        output_store_name = 'output_store'
+        output_store_name = "output_store"
         if isinstance(output_store, str):
             # if output_store is a string, it should be the a key to store_for_param
             output_store_name = output_store
             output_store = mall[output_store_name]
         else:
             # TODO: Assert MutableMapping, or just existence of __setitem__?
-            if not hasattr(output_store, '__setitem__'):
-                raise ValueError(f'Needs to have a __setitem__: {output_store}')
+            if not hasattr(output_store, "__setitem__"):
+                raise ValueError(f"Needs to have a __setitem__: {output_store}")
         if output_store_name in store_for_param:
             raise ValueError(
-                f'Name conflicts with existing param name: {output_store_name}'
+                f"Name conflicts with existing param name: {output_store_name}"
             )
 
         wrapped_f = store_on_output(
             wrapped_f,
             store=output_store,
             save_name_param=save_name_param,
-            add_store_to_func_attr='output_store' if include_stores_attribute else None,
+            add_store_to_func_attr="output_store" if include_stores_attribute else None,
             empty_name_callback=empty_name_callback,
             auto_namer=auto_namer,
             output_trans=output_trans,
@@ -546,35 +551,35 @@ def _mk_store_for_param(sig, param_to_mall_key_dict=None, mall=None, verbose=Tru
 
         warn(
             f"Some of your mall keys were also func arg names, but you didn't mention "
-            f'them in param_to_mall_map, namely, these: {unmentioned_mall_keys}'
+            f"them in param_to_mall_map, namely, these: {unmentioned_mall_keys}"
         )
     if param_to_mall_key_dict:
-        func_name_stub = ''
+        func_name_stub = ""
         if sig.name:
-            func_name_stub = f'({sig.name})'
+            func_name_stub = f"({sig.name})"
         if isinstance(param_to_mall_key_dict, str):
             param_to_mall_key_dict = param_to_mall_key_dict.split()
         if not set(param_to_mall_key_dict).issubset(sig.names):
             offenders = set(param_to_mall_key_dict) - set(sig.names)
             raise ValueError(
-                'The param_to_mall_map should only contain keys that are '
+                "The param_to_mall_map should only contain keys that are "
                 f"parameters (i.e. 'argument names') of your function {func_name_stub}. "
-                f'Yet these param_to_mall_map keys were not argument names: '
-                f'{offenders}'
+                f"Yet these param_to_mall_map keys were not argument names: "
+                f"{offenders}"
             )
         if not set(param_to_mall_key_dict.values()).issubset(mall.keys()):
             offenders = set(param_to_mall_key_dict.values()) - set(mall.keys())
-            keys = 'keys' if len(offenders) > 1 else 'key'
-            offenders = ', '.join(map(lambda x: f"'{x}'", offenders))
+            keys = "keys" if len(offenders) > 1 else "key"
+            offenders = ", ".join(map(lambda x: f"'{x}'", offenders))
 
             raise ValueError(
-                f'The {offenders} {keys} of your param_to_mall_map values were not '
-                f'in the mall. Your param_to_mall_key_dict is:\n'
-                f'{param_to_mall_key_dict} and your mall has keys: {list(mall)}. '
-                f'You can either add {offenders} stores to the mall, '
+                f"The {offenders} {keys} of your param_to_mall_map values were not "
+                f"in the mall. Your param_to_mall_key_dict is:\n"
+                f"{param_to_mall_key_dict} and your mall has keys: {list(mall)}. "
+                f"You can either add {offenders} stores to the mall, "
                 # f'make an auto-store-making mall (e.g. collections.defaultdict(dict), '
-                f'or change your param_to_mall_map to point (values) to a store that '
-                f'you actually have in the mall'
+                f"or change your param_to_mall_map to point (values) to a store that "
+                f"you actually have in the mall"
             )
         # Note: store_for_param used to be the argument of prepare_for_crude_dispatch,
         #   instead of the (param_to_mall_map, mall) pair which is overkill.
@@ -670,11 +675,11 @@ def simple_mall_dispatch_core_func(
         if not action:
             return store
 
-    key = key or ''
-    if action == 'list':
+    key = key or ""
+    if action == "list":
         key = key.strip()  # to handle some invisible whitespace that would screw things
         return list(filter(lambda k: key in k, store))
-    elif action == 'get':
+    elif action == "get":
         return store[key]
 
 
@@ -686,7 +691,9 @@ from i2 import Sig, Pipe
 from i2.signatures import sig_to_dataclass
 
 _Crudifier = sig_to_dataclass(
-    Sig(prepare_for_crude_dispatch).params[1:], cls_name='_Crudifier', module=__name__,
+    Sig(prepare_for_crude_dispatch).params[1:],
+    cls_name="_Crudifier",
+    module=__name__,
 )
 
 
@@ -816,6 +823,89 @@ class Crudifier(_Crudifier):
     def __call__(self, func):
         # is there a safer way than vars to get the init fields (keys and values)?
         return prepare_for_crude_dispatch(func, **vars(self))
+
+
+from i2 import name_of_obj, Sig
+from dol import chain_get
+
+# Note: Could use function objects themselves as keys instead of (or along with) function names. Pros/cons?
+
+# alt arg names:
+# def crudify_based_on_names(func, *, input_store_for_arg=(), output_store_for_func_name=()):  pass
+
+
+def _remove_non_valued_items(d: dict):
+    return {k: v for k, v in d.items() if v is not None}
+
+
+def _keys_to_search(func):
+    """Function defining what forms of keys will be searched in the arg_input_stores
+    when using crudify_based_on_names on a function.
+    Note that since chain_get will be used on this, it's the first key found that will be used,
+    making, for example, a ``(func, arg_name)`` specification have precedence over an `arg_name` specification"""
+    func_name = name_of_obj(func)
+    for arg_name in Sig(func).names:
+        yield (
+            arg_name,
+            (
+                (func, arg_name),
+                (func_name, arg_name),
+                f"{func_name}.{arg_name}",
+                arg_name,
+            ),
+        )
+
+
+def crudify_based_on_names(
+    func, *, arg_input_stores=(), func_output_stores=(), crudifier=Crudifier
+):
+    """
+    Crudify a function based on general
+
+    :param func:
+    :param arg_input_stores:
+    :param func_output_stores:
+    :param crudifier:
+    :return:
+
+    >>> from functools import partial
+    >>> def foo(x, y):
+    ...     return x + y
+    >>> def bar(a, x):
+    ...     return a * x
+    >>> general_crudifier = partial(
+    ...     crudify_based_on_names,
+    ...     arg_input_stores={'x': 'x_store'},
+    ...     crudifier=partial(prepare_for_crude_dispatch, mall={'x_store': {'stored_two': 2, 'stored_four': 4}})
+    ... )
+    >>>
+    >>> foo, bar = map(general_crudifier, [foo, bar])
+    >>>
+    >>> foo('stored_two', 10)
+    12
+    >>> bar(4, 'stored_four')
+    16
+
+    """
+    arg_input_stores = dict(arg_input_stores)
+    func_output_stores = dict(func_output_stores)
+    func_name = name_of_obj(func)
+    param_to_mall_map = (
+        _remove_non_valued_items(
+            {
+                arg_name: chain_get(arg_input_stores, keys)
+                for arg_name, keys in _keys_to_search(func)
+            }
+        )
+        or None
+    )
+    output_store = chain_get(func_output_stores, (func, func_name), default=None)
+    if param_to_mall_map or output_store:
+        return crudifier(
+            func, param_to_mall_map=param_to_mall_map, output_store=output_store
+        )
+    else:
+        return func
 
 
 # ---------------------------------------------------------------------------------------
